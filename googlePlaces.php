8aa97eb21fc8423cf6572f9cfa8de7ae62033cc6
Yancheng <405138209@qq.com>
2014-09-09 15:24:42 +1000

    Update googlePlaces.php

--- googlePlaces.php
+++ googlePlaces.php
@@ -1,3 +1,4 @@
+<?php
 //Google Places API PHP Wrapper
 //Copyright (C) 2011 - 2014 Anthony Mills ( me@anthony-mills.com )
 //
@@ -14,6 +15,7 @@
 //You should have received a copy of the GNU General Public License
 //along with this program; if not, write to the Free Software
 //Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+?>
 <?php
 
 class googlePlaces {
