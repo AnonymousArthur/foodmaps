89ab0c465dcf05cc723dd2475ea0eeecaa88ef7c
Jack Holt <holty_222@hotmail.com>
2014-09-23 01:04:32 +1000

    Update GooglePlaceText.php

--- GooglePlaceText.php
+++ GooglePlaceText.php
@@ -13,6 +13,8 @@ $_SESSION['views']=1;
 <link rel="stylesheet" href="feedme.css">
 </header>
 <body id='main'>
+<div id="container" class="js-masonry"
+  data-masonry-options='{ "columnWidth": 150, "itemSelector": ".item" }'>
 <?php include('title.html');?>
 <?php
 require_once('googlePlaces.php');
@@ -27,22 +29,26 @@ $googlePlaces->setQuery($keyword);
 $results = $googlePlaces->textSearch();
 foreach($results['result'] as $information){
 	//$linkName=str_replace(' ', '+', $information['name'])
-	echo "<a href='details.php?restaurant=",$information['name'],"&suburb=",$searchArea,"'>",$information['name'],"</a>";
-	$_SESSION[$information['name']]['Address']=$information['formatted_address'];
-	$_SESSION[$information['name']]['icon']=$information['icon'];
-	echo '<br/>';
+	
 	if(!empty($information['photos'])){
 		foreach($information['photos'] as $p){
 			if(!empty($p['photo_reference'])){
+				echo '<div class="item">';
+				echo "<a href='details.php?restaurant=",$information['name'],"&suburb=",$searchArea,"'>",$information['name'],"</a>";
+				$_SESSION[$information['name']]['Address']=$information['formatted_address'];
+				$_SESSION[$information['name']]['icon']=$information['icon'];
+				echo '<br/>';
 				$URL = $googlePlaces->photo($p['photo_reference'],$p['height'],$p['width']);
 				echo '<img src="',$URL,'">';
 				echo '<br/><br/>';
+				echo '</div>';	
 			}
 		}
 	}
 }
 
 ?>
+</div>
 <a href="index.php"><button class="btn btn-default" type="button">Back</button></a>
 <?php include('footer.html');?>
 </body>
