5c9139667329dfd51586e048cb97c15b7bacdc79
Yancheng <unicrix@gmail.com>
2014-09-23 16:02:03 +1000

    Merged code

--- GooglePlaceText.php
+++ GooglePlaceText.php
@@ -17,15 +17,11 @@ $_SESSION['views']=1;
 require_once('googlePlaces.php');
 $searchArea=$_GET['location'];
 echo '<p>Restaurants near '.$searchArea.'</p>';
-<<<<<<< HEAD
-$apiKey       = '';
 ?>
 <div id="container" class="js-masonry"
   data-masonry-options='{ "columnWidth": 150,"itemSelector": ".item" }'>
 <?php
-=======
-$apiKey       = 'IzaSyD_Ss7yu2RVDdflnls4vaknndHIsfIqUBE';
->>>>>>> FETCH_HEAD
+$apiKey       = 'AIzaSyBMGzm-1zXeLX-QNi9N7ozRJWZlREyYEe4';
 $googlePlaces = new googlePlaces($apiKey);
 
 $googlePlaces->setRadius(2000);
